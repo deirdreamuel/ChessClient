2c05de88f1447ee86d32e79acb64d8a92392bc4a
Deirdre Chong <xerzen@live.com>
2022-01-08 21:59:06 -0800

    front end initial project

---
+++ components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card outlined max-width="500px">
+    <v-card-title>LOG IN</v-card-title>
+    <v-list-item>
+      <v-list-item-content>
+        <Textfield label="EMAIL" :val="username"></Textfield>
+        <Textfield
+          :show="false"
+          :showIcon="true"
+          label="PASSWORD"
+          :val="password"
+        ></Textfield>
+        <v-btn block text> LOG IN </v-btn>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
+</template>
+
+<script>
+import { defineComponent, ref } from "@nuxtjs/composition-api";
+import Textfield from "~/components/fields/textfield.vue";
+
+export default defineComponent({
+  name: "Login",
+  components: { Textfield },
+  setup() {
+    const username = ref("GUEST")
+    const password = ref("password")
+
+    return {
+      username,
+      password,
+    }
+  }
+})
+</script>
+
+<style>
+</style>
---
+++ components/navigation/AppBar.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed flat app dense
+      class="px-6"
+    >
+        <v-toolbar-title v-text="title" class="appTitle"/>
+        <v-spacer />
+
+        <ProfileBar />
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from '@nuxtjs/composition-api'
+import ProfileBar from '~/components/navigation/ProfileBar.vue'
+
+export default defineComponent({
+    name: 'AppBar',
+    components: { ProfileBar },
+    setup () {
+        const title = ref('CHESS')
+        const clipped = ref(false)
+        const drawer = ref(false)
+        const fixed = ref(false)
+        const miniVariant = ref(false)
+        const right = ref(false)
+
+        return {
+            title,
+            clipped,
+            drawer,
+            fixed,
+            miniVariant,
+            right,
+        }
+    }
+})
+</script>
+
+<style scoped>
+.appTitle {
+    font-family: Helvetica, sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+}
+.appMenu {
+    font-family: Helvetica, sans-serif;
+    font-size: 16px;
+    font-weight: bold;
+    color: black;
+}
+</style>
